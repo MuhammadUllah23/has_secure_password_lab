bf628ba8f54da4bf22144cfc182a2a51d182326f
Muhammad <muhammadhullah23@gmail.com>
2021-07-25 18:47:20 -0400

    commit tree 7f238f571be80d67b2f625868c65d4ec8e05b0c0

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+def new
+end
+
+def create
+
+    user = User.find_by(name: params[:user][:name])
+    authenticated = user.try(:authenticate, params[:user][:password])
+    return head(:forbidden) unless authenticated
+    @user = user
+    session[:user_id] = @user.id
+    redirect_to root_path
+end
+
+end
